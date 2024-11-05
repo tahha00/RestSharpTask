222e5e91814c8b888e44410c4db24075b472e253
tahha00 <tahha2k9@gmail.com>
2024-11-05 15:18:27 +0000

    RestSharp mini task first commit

---
+++ RestSharp/Features/products.feature
@@ -0,0 +1,7 @@
+﻿Feature: products
+
+A short summary of the feature
+
+Scenario: Get products list
+	When I get all the products
+	Then I get a response code '200'
---
+++ RestSharp/StepDefinitions/Products_Steps.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System;
+using TechTalk.SpecFlow;
+
+namespace RestSharpProject.StepDefinitions
+{
+    [Binding]
+    public class Product_Steps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        RestClient _client;
+        RestResponse _response;
+
+        public Product_Steps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [When(@"I get all the products")]
+        public void WhenIGetAllTheProducts()
+        {
+            _client = new RestClient("http://localhost:2002/api/");
+            RestRequest request = new RestRequest("products", Method.Get) { RequestFormat = DataFormat.Json };
+            _response = _client.Execute(request);
+        }
+
+        [Then(@"I get a response code '(.*)'")]
+        public void ThenIGetAResponseCode(int responseCode)
+        {
+            int status = (int)_response.StatusCode;
+            Assert.That(status, Is.EqualTo(responseCode), "Status codes do not match");
+        }
+    }
+}
+
+
